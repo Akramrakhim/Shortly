1a2a1980de2ee3f93386830c01f6b995977c83ee
Akramrakhim <akramrakhimov4@gmail.com>
2023-04-08 23:20:33 +0500

    Add url copy button click handler wrap form submit handler in if

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
+const elUrlShortener = document.querySelector('.url-shortener');
 const elUrlShortenerForm = document.querySelector('.js-url-shortener-form');
 const elUrlShortenerResults = document.querySelector('.url-shortener__results');
 
-elUrlShortenerForm.addEventListener('submit', function(evt){
-    evt.preventDefault();
-    elUrlShortenerResults.classList.add('url-shortener__results--open')
-});
+if(elUrlShortenerForm)  {
+    elUrlShortenerForm.addEventListener('submit', function (evt){
+        evt.preventDefault();
+        elUrlShortenerResults.classList.add('url-shortener__results--open')
+    });
+}   
+
+if(elUrlShortener){
+    elUrlShortener.addEventListener('click', function (evt) {
+        if(evt.target.matches('.js-copy-short-link-button')){
+            evt.target.textContent = 'Copied!';
+            evt.target.classList.add('url-shortener__copy-button--copied');
+            setTimeout(function (){
+                evt.target.textContent = 'Copy';
+                evt.target.classList.remove('url-shortener__copy-button--copied');
+            },1000);
+        }
+    }); 
+}
+
